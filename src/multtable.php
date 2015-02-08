0ac98dd6af5eec67db139fd797f94c9c6ad655ca
Eric Rouse <rousee@onid.oregonstate.edu>
2015-02-08 12:24:08 -0800

    checking the max/min relationships

--- src/multtable.php
+++ src/multtable.php
@@ -23,23 +23,45 @@ We require four integer parameters passed via HTTP GET
 //print all the keys and values if debug is on
 if(isset($_GET['debug'])){
     foreach($_GET as $key => $value){
-        echo "KEY $key, VALUE $value" . "<br>";
+        echo "KEY $key, VALUE $value <br>";
     }
 }
-//take an input and convert to an integer, if possible
-
-$input_params = array("min-multiplicand", "max-multiplicand", "min-multiplier", "max-multiplier");
-foreach ($input_params as $parm){
+//sanitize the inputs from the GET
+//if all the inputs exist and are integers, fill the output array with them
+//otherwise let the user know about their sucky input
+$input_parms = array("min-multiplicand", "max-multiplicand", "min-multiplier", "max-multiplier");
+$output_parms = array();
+$errorIn_parms = False;
+foreach ($input_parms as $parm){
     if(isset($_GET[$parm])){
         if (ctype_digit($_GET[$parm])){
-            echo "<br> YOU DID IT! <br>";
+            $output_parms[$parm] = intval($_GET[$parm]);
         } else {
-            echo "$parm must be an integer >= 0";
+            echo "$parm must be an integer >= 0<br>";
+            $errorIn_parms = True;
         }
     } else {
         echo "missing parameter: $parm<br>";
+        $errorIn_parms = True;
     }
 }
+
+//if data passed in correctly, act on it
+if (!$errorIn_parms){
+    echo "YOU DID IT!<br>";
+    //check that min is <= max
+    if ($output_parms['min-multiplicand'] > $output_parms['max-multiplicand']) {
+        echo "minimum multiplicand is larger than maximum<br>";
+        $errorIn_parms = True;
+    }
+
+    if ($output_parms['min-multiplier'] > $output_parms['max-multiplier']) {
+        echo "minimum multiplier is larger than maximum<br>";
+        $errorIn_parms = True;
+    }
+}
+
+//if still no error, process table
 
 
 ?>
