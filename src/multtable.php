6a29518309c2b16ddfcf4da4b4ba88511a373173
Eric Rouse <rousee@onid.oregonstate.edu>
2015-02-08 11:22:17 -0800

    starting multtable.php

---
+++ src/multtable.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>multtable PHP example</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+echo $foo = 5 + 12 . "this";
+
+
+?>
