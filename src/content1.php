5eba645ebc341bef7f35da2bc85c71e87d5a90fd
Eric Rouse <rousee@onid.oregonstate.edu>
2015-02-08 16:37:33 -0800

    http_redirect didn't work, need special PECL, created header redirect helper

--- src/content1.php
+++ src/content1.php
@@ -3,20 +3,32 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 session_start();
+function help_redirect($url)
+{
+    if (headers_sent() === false)
+    {
+        header('Location: ' . $url, true);
+    }
+}
+
+
 function killsession(){
     $_SESSION = array();
     session_destroy();
-    http_redirect("./login.php");
+    $path = explode('/', $_SERVER['PHP_SELF'], -1);
+    $path = implode('/', $path);
+    $path = "http://$_SERVER[HTTP_HOST]$path/login.php";
+    help_redirect($path);
     die();
 }
-echo '<!DOCTYPE html>
+
+$hdr = '<!DOCTYPE html>
         <html lang = "en">
           <head>
             <meta charset="UTF-8">
             <title>content 1 PHP</title>
           </head>
           <body>';
-
 //verify username 
 if(isset($_POST['username']) && $_POST['username'] != null) {
     if(session_status() == PHP_SESSION_ACTIVE){
@@ -32,19 +44,22 @@ if(isset($_POST['username']) && $_POST['username'] != null) {
         }
     }
     
-} elseif(isset($_POST['logout']) && $_POST['logout'] == "Logout"){
-    echo "Logged Out. Click <a href=".'"./login.php">'."here</a> to return to login screen, or wait to be redirected.";
+} elseif(isset($_POST['logout']) && $_POST['logout'] == "Logout") {
+    /*echo "Logged Out. Click <a href=".'"./login.php">'."here</a> to return to login screen, or wait to be redirected.";*/
     killsession();
 
 
+} elseif (isset($_POST['username']) && $_POST['username'] == null) {
+    echo "$hdr A username must be entered. Click <a href=".'"./login.php">'."here</a> to return to login screen.";
+    killsession();
 }
+
 //session stuff...
 if(isset($_SESSION['active']) && $_SESSION['active']) {
-    echo "Hello $_SESSION[username], you have visited this page $_SESSION[c1visits] times.<br>";
+    echo "$hdr Hello $_SESSION[username], you have visited this page $_SESSION[c1visits] times.<br>";
     echo "Click <a href=".'"./content2.php">'."here</a> for content2.";
     $_SESSION['c1visits']++;
 } else {
-    echo "A username must be entered. Click <a href=".'"./login.php">'."here</a> to return to login screen, or wait to be redirected.";
     killsession();
 }
 ?>
--- src/content2.php
+++ src/content2.php
@@ -3,25 +3,38 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 session_start();
+function help_redirect($url)
+{
+    if (headers_sent() === false)
+    {
+        header('Location: ' . $url, true);
+    }
+}
+
 function killsession(){
     $_SESSION = array();
     session_destroy();
+    $path = explode('/', $_SERVER['PHP_SELF'], -1);
+    $path = implode('/', $path);
+    $path = "http://$_SERVER[HTTP_HOST]$path/login.php";
+    help_redirect($path);
     die();
 }
-echo '<!DOCTYPE html>
+
+$hdr = '<!DOCTYPE html>
         <html lang = "en">
           <head>
             <meta charset="UTF-8">
-            <title>content 2 PHP</title>
+            <title>content 1 PHP</title>
           </head>
           <body>';
+
 //session stuff...
 if(isset($_SESSION['active']) && $_SESSION['active']){
-    echo "Hello $_SESSION[username], you have visited this page $_SESSION[c2visits] times.<br>";
+    echo "$hdr Hello $_SESSION[username], you have visited this page $_SESSION[c2visits] times.<br>";
     echo "Click <a href=".'"./content1.php">'."here</a> for content1.";
     $_SESSION['c2visits']++;
 } else {
-    echo "A username must be entered. Click <a href=".'"./login.php">'."here</a> to return to login screen.";
     killsession();
 }
 ?>
